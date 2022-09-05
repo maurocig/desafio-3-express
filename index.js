799c26ff2f8af438da5053e352694b9350524e8d
Mauro Cigliuti <mcigliuti01@gmail.com>
2022-09-05 10:36:45 -0300

    ok

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const app = express();
-const archivo = './productos.txt';
+const fs = require('fs/promises');
+const Container = require('./Contenedor');
+
 
 const PORT = process.env.PORT || 8080;
 
-app.get('/productos', () => {
-	console.log(archivo[0])
+const productos = new Container('productos.txt');
+
+async function getFile() {
+	try {
+		const string = await fs.readFile('./productos.txt', 'utf-8');
+		const file = JSON.parse(string);
+		return file;
+
+	}
+	catch (error) {
+		console.log(error)
+	}
+}
+
+app.get('/productos', async (req, res) => {
+	const file = await productos.getFile();
+	res.send(file);
+})
+
+app.get('/productoRandom', async (req, res) => {
+	const arrayProductos = await productos.getFile();
+	const numeroRandom = Math.floor(Math.random() * (arrayProductos.length));
+	console.log(numeroRandom);
+	res.send(arrayProductos[numeroRandom]);
 })
 
 app.listen(PORT, () => {
